5e6d417c39295559a75a085e03a4e88b32630922
Jannik Aumann <82949423+Ic3Gam3s@users.noreply.github.com>
2023-09-05 14:43:40 +0200

    Insert Coding Files

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * Beschreibung:
+ * BMI Rechner
+ *
+ * @version 1.0 vom 29.08.2023
+ * @author 
+ */
+
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    
+    
+  } // end of main
+
+} // end of class Main
